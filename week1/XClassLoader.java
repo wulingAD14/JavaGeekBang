fd7442fc25b9cfdb87304429e6807c89ffd36438
GeekLin <121437225@qq.com>
2021-08-08 21:02:30 +0800

    week1:hello.xlass:v2

--- week1/XClassLoader.java
+++ week1/XClassLoader.java
@@ -3,18 +3,25 @@ package week1;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class XClassLoader extends ClassLoader{
 
     public static void main(String[] args){
         try{
             Object obj = new XClassLoader().findClass("Hello").newInstance();
-            //obj.hello();
+            Method m = obj.getClass().getMethod("hello",null);
+            m.invoke(obj);
         }  catch (ClassNotFoundException e){
             e.printStackTrace();
         } catch (IllegalAccessException e){
             e.printStackTrace();
         } catch (InstantiationException e){
+            e.printStackTrace();
+        } catch (NoSuchMethodException e){
+            e.printStackTrace();
+        } catch (InvocationTargetException e){
             e.printStackTrace();
         }
     }
